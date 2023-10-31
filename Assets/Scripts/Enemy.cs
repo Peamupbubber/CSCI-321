4d2681cc87d3ac895d57c73f5c7fe5a76d7a2d77
Hunter Smith <hunteralansmith@outlook.com>
2023-10-31 10:49:00 -0700

    ai

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float fovAngle = 110f;
+    [HideInInspector] public bool playerInSight = false;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            Debug.Log("Player in range...");
+            RaycastHit hit;
+            Vector3 enemyToPlayer = other.gameObject.transform.position - transform.position;
+            float angleToPlayer = Vector3.Angle(enemyToPlayer, transform.forward);
+            bool isAngleUnderHalfAngleOfView = angleToPlayer < fovAngle * 0.5f;
+
+            if (isAngleUnderHalfAngleOfView && Physics.Raycast(transform.position + transform.up, enemyToPlayer.normalized, out hit, 8)) {
+                playerInSight = true;
+                Debug.Log("Player sighted!");
+            }
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            playerInSight = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,73 @@
+// Patrol.cs
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class Patrol : MonoBehaviour
+{
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    
+    private Animator anim;
+    private Enemy enemy;
+    private GameObject player;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+        anim = GetComponent<Animator>();
+        enemy = GetComponent<Enemy>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+    void Update()
+    {
+        if (ShouldChasePlayer())
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            // Choose the next destination point when the agent gets
+            // close to the current one.
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+                GotoNextPoint();
+        }
+        anim.SetFloat("Speed", agent.velocity.sqrMagnitude);
+    }
+
+    private bool ShouldChasePlayer() {
+        return enemy.playerInSight;
+    }
+
+    private void ChasePlayer() {
+        agent.SetDestination(player.transform.position);
+    }
+}
